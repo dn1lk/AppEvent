6783831da0e6dafed346eeb48663125bdf823853
Шамраев Данила <d.shamraev@appevent.ru>
2023-04-08 19:19:01 +0300

    refactoring

--- 17.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env perl
-use strict;
-
-my $number = 12345678.09;
-
-$number = sprintf "%.2f", $number;
-while ($number =~ s/(.*\d)(\d\d\d)/$1\'$2/g) {};  # каюсь, просто скопировал с интернетов
-
-printf $number;
--- 6.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env perl
-use strict;
-
-my @list = ('A'..'Z', 'a'..'z', '0'..'9');
-my $key;
-
-for (1..10) {$key .= @list[rand @list]}
-
-print $key;
